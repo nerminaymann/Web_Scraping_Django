1aea2c98c4f9fe7d9cff00772026c8f139fd2bba
nerminaymann <nerminayman1452@gmail.com>
2024-10-20 12:57:01 +0300

    Converted FBV to CBV

--- web_scraping_django/core/views.py
+++ web_scraping_django/core/views.py
@@ -1,15 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
 from core.services.scraper_service import WebScraper, ProductParser
-def home(request):
-    products_list_info = []
-    if 'product' in request.GET:
-        product_query = request.GET.get('product')
-        base_url = "https://www.jumia.com.eg"
 
-        # Parse the product data
-        parser = ProductParser(base_url,product_query)
-        products_list_info = parser.parse_products()
+class HomeView(TemplateView):
+    template_name = 'core/home.html'
 
-    return render(request, 'core/home.html', {'products_list_info': products_list_info})
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        products_list_info = []
+
+        if 'product' in self.request.GET:
+            product_query = self.request.GET.get('product')
+            base_url = "https://www.jumia.com.eg"
+
+            # Parse the product data
+            parser = ProductParser(base_url, product_query)
+            products_list_info = parser.parse_products()
+
+        context['products_list_info'] = products_list_info
+        return context
